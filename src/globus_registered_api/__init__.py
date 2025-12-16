af0073f1923832d9fc7619fcd59820d553afbe6a
m1yag1 <8730430+m1yag1@users.noreply.github.com>
2025-12-16 15:50:24 -0600

    Add `ExtendedFlowsClient` with `list_registered_apis` method
    Extend `FlowsClient` with a method to list registered APIs for which the
    caller has a role. Supports `filter_role`, `filter_roles`, `orderby`,
    `per_page`, and `marker` parameters.

---
+++ src/globus_registered_api/__init__.py
@@ -0,0 +1,8 @@
+# This file is a part of globus-registered-api.
+# https://github.com/globusonline/globus-registered-api
+# Copyright 2025 Globus <support@globus.org>
+# SPDX-License-Identifier: Apache-2.0
+
+from globus_registered_api.extended_flows_client import ExtendedFlowsClient
+
+__all__ = ["ExtendedFlowsClient"]
