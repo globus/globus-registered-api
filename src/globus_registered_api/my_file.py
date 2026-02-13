b04ec53a080ccc2a67adc6a467218efd9fd209e0
derek-globus <derek@globus.org>
2026-02-13 11:06:05 -0600

    Added a fake file

---
+++ src/globus_registered_api/my_file.py
@@ -0,0 +1,7 @@
+# This file is a part of globus-registered-api.
+# https://github.com/globus/globus-registered-api
+# Copyright 2025-2026 Globus <support@globus.org>
+# SPDX-License-Identifier: Apache-2.0
+
+
+def my_func(): ...
