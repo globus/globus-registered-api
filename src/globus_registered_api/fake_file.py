c5b4f78779d10b3de02c5f0087be42f7c12cfc79
derek-globus <derek@globus.org>
2026-02-17 12:03:29 -0600

    Add some fake files

---
+++ src/globus_registered_api/fake_file.py
@@ -0,0 +1,8 @@
+# This file is a part of globus-registered-api.
+# https://github.com/globus/globus-registered-api
+# Copyright 2025-2026 Globus <support@globus.org>
+# SPDX-License-Identifier: Apache-2.0
+
+
+def my_function() -> None:
+    return "This is a fake file for testing purposes."
